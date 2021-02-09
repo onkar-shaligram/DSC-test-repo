a81f2f12a619d5c43762e23554d38ba12f4f43ea
onkar-shaligram <onkarshaligram01@gmail.com>
2021-02-09 21:16:00 +0530

    Added request event form

--- lib/screens/dscScreen/requestEvent.dart
+++ lib/screens/dscScreen/requestEvent.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dsc_local/configs/assets.dart';
 import 'package:flutter/material.dart';
 
@@ -7,6 +8,29 @@ class RequestEventForm extends StatefulWidget {
 }
 
 class _RequestEventFormState extends State<RequestEventForm> {
+  TextEditingController nameEditingController = TextEditingController();
+  TextEditingController eventEditingController = TextEditingController();
+  TextEditingController guestnameEditingController = TextEditingController();
+  TextEditingController guestLinkEditingController = TextEditingController();
+
+  addToFirestore() async {
+    Map<String, dynamic> eventData = {
+      "Person's name": nameEditingController.text,
+      "Event name": eventEditingController.text,
+      "Guest name": guestnameEditingController.text,
+      "Guest url": guestLinkEditingController.text,
+    };
+
+    FirebaseFirestore.instance
+        .collection("Event Requests")
+        .add(eventData)
+        .catchError((e) {
+      print("Error encountered while uploading data : $e");
+    });
+
+    Navigator.pop(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,13 +44,52 @@ class _RequestEventFormState extends State<RequestEventForm> {
                 child: Center(child: Image.asset(Assets.vitb_dsc_logo)),
               ),
             ),
-            SizedBox(height: 20,),
+            SizedBox(
+              height: 20,
+            ),
             Expanded(
-              flex: 8,
-              child: Container(
-                
+              flex: 6,
+              child: SingleChildScrollView(
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: nameEditingController,
+                        decoration:
+                            InputDecoration(hintText: "Enter your Name"),
+                        maxLines: 1,
+                      ),
+                      TextField(
+                        controller: eventEditingController,
+                        decoration: InputDecoration(
+                            hintText: "On What Topic do you need an event??"),
+                        maxLines: 2,
+                      ),
+                      TextField(
+                        controller: guestnameEditingController,
+                        decoration: InputDecoration(
+                            hintText: "Any particular guest you want to call?"),
+                        maxLines: 1,
+                      ),
+                      TextField(
+                        controller: guestLinkEditingController,
+                        decoration: InputDecoration(
+                            hintText: "Enter url to guest's public profile"),
+                        maxLines: 2,
+                      ),
+                      SizedBox(height: 30,),
+                      RaisedButton(
+                        onPressed: () {
+                          addToFirestore();
+                        },
+                        child: Text("submit"),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-            )
+            ),
           ],
         ),
       ),
